128f91dda7e0b945764d92c13aa1296cd418e92c
Thanh Nhan <thanhnhanln97@gmail.com>
2018-07-01 13:25:25 +0700

    delete gabit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,6 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
-import { ChildComponent } from './child/child.component';
-import { UsersComponent } from './users/users.component';
 import { HttpClientModule } from '@angular/common/http';
 import { UserComponent } from './dashboard/user/user.component'
 import { ProductsComponent } from './dashboard/products/products.component';
@@ -17,8 +15,6 @@ import { routes } from './shared/app-routes.routing';
 @NgModule({ 
   declarations: [
     AppComponent,
-    ChildComponent,
-    UsersComponent,
     UserComponent,
     ProductsComponent,
     ProductcategoriesComponent,
