b0b1fc660ea1672524b21c52664e89391c881c26
Duong Minh <duongdmv12@Duong-Minh.local>
2018-07-01 16:30:37 +0700

    CRUD Product Component, (don't use service and API)

--- src/app/dashboard/products/products.component.ts
+++ src/app/dashboard/products/products.component.ts
@@ -1,4 +1,8 @@
+import { Observable, of } from 'rxjs';
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Product } from '../../shared/models/product';
+import { Productcategory } from '../../shared/models/productcategory';
 
 @Component({
   selector: 'app-products',
@@ -7,9 +11,93 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductsComponent implements OnInit {
 
-  constructor() { }
+  listProduct$: Observable<Product[]> = of([]);
+  productForm: FormGroup;
+  category$: Observable<Productcategory[]> = of([]);
+  message: string = null;
+  constructor(
+    private builder: FormBuilder
+  ) {
+    this.createProductForm();
+  }
 
   ngOnInit() {
+  }
+
+  createProductForm() {
+    this.productForm = this.builder.group({
+      ProductID: [],
+      ProductCategoryID: [],
+      ProductName: ['', [Validators.required]],
+      ProductPrice: ['1'],
+      ProductLocation: [],
+      ProductImage: []
+    });
+  }
+
+  addItem() {
+    this.listProduct$.forEach(
+      data => {
+        data.push(this.productForm.value);
+      }
+    );
+    this.showMessageBox('Add Item Success');
+  }
+
+  deleteItem(index: number) {
+    this.listProduct$.forEach(
+      data => {
+        data.splice(index,1);
+      }
+    );
+    this.showMessageBox('Delete Item Success');
+  }
+
+  updateItem() {
+    this.listProduct$.forEach(
+      data => {
+        let index = data.findIndex( x => x.ProductID === this.productForm.controls['ProductID'].value);
+        data[index] = this.productForm.value;
+      }
+    );
+    this.showMessageBox('Update success');
+  }
+
+  pathValueItem(item: Product) {
+    this.productForm.patchValue({
+      ProductCategoryID: item.ProductCategoryID,
+      ProductName: item.ProductName,
+      ProductPrice: item.ProductPrice,
+      ProductLocation: item.ProductLocation,
+      ProductID: item.ProductID
+    });
+  }
+
+  uploadFile($event: FileList) {
+    if ($event) {
+
+      let url = `assets/imgs/${$event.item(0).name}`;
+      this.productForm.patchValue({
+        ProductImage: url
+      });
+    }
+
+  }
+
+  selectChagneItem($event) {
+    this.productForm.patchValue({
+      ProductCategoryID: $event
+    });
+  }
+
+  showMessageBox(message: string)
+  {
+    this.message = message;
+    setTimeout(
+      () => {
+        this.message = null;
+      }, 3000
+    );
   }
 
 }
